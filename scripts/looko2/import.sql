024c76163868abca5bd282adf20e61cc58b20374
Damian Kuś <damian.kus.main@gmail.com>
2017-12-18 23:14:17 +0100

    added sql script for importing agh meteo and looko2 data

---
+++ scripts/looko2/import.sql
@@ -0,0 +1,46 @@
+﻿-- 2016-03-07	8	7	3	2016	18FE34CF51A3	43.2105	55.9474	63.9474	KRAKOW_MYDLNIKI
+-- (date, hour, day, month, year, station_code, pm1, pm2_5, pm10, station_name)
+
+DROP TABLE observations;
+CREATE TABLE observations (
+    id SERIAL PRIMARY KEY,
+    date DATE,
+    hour NUMERIC(2),
+    day NUMERIC(2),
+    month NUMERIC(2),
+    year NUMERIC(4),
+    station_id CHAR(15),
+    pm1 NUMERIC(8, 4),
+    pm2_5 NUMERIC(8, 4),
+    pm10 NUMERIC(8, 4),
+    station_name CHAR(50)
+);
+\copy observations (date, hour, day, month, year, station_id, pm1, pm2_5, pm10, station_name) FROM 'looko2.csv' WITH HEADER DELIMITER ',' CSV;
+
+
+DROP TABLE stations;
+SELECT station_id as id, station_name
+INTO stations
+FROM 
+(
+	SELECT DISTINCT station_id, station_name, lower(station_name) as name
+	FROM observations
+	ORDER BY station_id
+) AS krk_stations
+WHERE name LIKE '%krakow%'
+OR name LIKE '%krk%'
+OR name LIKE '%ruczaj%'
+OR name LIKE '%podhalanska%'
+OR name ='przedszkolegalaktyka3'
+OR name = 'smogoweinfotest'
+OR name ='LOOKO2_AC117'
+OR name ='poddebowiec'
+OR name ='ww_piaski_v3_atmo'
+ORDER BY station_name;
+
+
+DELETE FROM observations
+WHERE station_name NOT IN 
+(
+	SELECT station_name FROM stations
+);
