472901baf0f590dd6295a105102267f63b11ad0b
Damian Kuś <damian.kus.main@gmail.com>
2018-04-14 19:13:53 +0200

    added titles to monthly distribution figures

--- analysis/yearly_trend/yearly_trend.r
+++ analysis/yearly_trend/yearly_trend.r
@@ -4,8 +4,7 @@ require('reshape')
 Sys.setenv(LANG = "en")
 
 cap <- function (s) {
-  s <- strsplit(s, ' ')[[1]]
-  paste(toupper(substring(s, 1, 1)), substring(s, 2), sep = '', collapse = ' ')
+  paste(toupper(substring(s, 1, 1)), substring(s, 2), sep = '')
 }
 
 units <- function (var) {
@@ -50,12 +49,12 @@ pretty_var <- function (var) {
          })
 }
 
-save_boxplot <- function (df, factor, plot_path) {
+save_boxplot <- function (df, factor, plot_path, title) {
   plot <- ggplot(data = df) +
     geom_boxplot(aes_string(x = 'date', y = factor)) +
+    ggtitle(title) +
     xlab('Date') +
-    ylab(cap(
-      paste(pretty_var(factor), '[', units(factor),']', sep = ' '))) +
+    ylab(cap(paste(pretty_var(factor), '[', units(factor),']', sep = ' '))) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1))
   ggsave(plot_path, width = 16, height = 10, dpi = 200)
   print(paste('Plot saved in', plot_path, sep = ' '))
@@ -72,7 +71,7 @@ save_lineplot <- function (df, factor, plot_path) {
   print(paste('Plot saved in', plot_path, sep = ' '))
 }
 
-save_histogram <- function (df, factor, plot_path) {
+save_histogram <- function (df, factor, plot_path, title) {
   # The bin width is calculated using the Freedman-Diaconis formula
   # See: https://stats.stackexchange.com/questions/798/calculating-optimal-number-of-bins-in-a-histogram
   # Also: https://nxskok.github.io/blog/2017/06/08/histograms-and-bins/
@@ -83,8 +82,9 @@ save_histogram <- function (df, factor, plot_path) {
   
   plot <- ggplot(data = df, aes_string(fact_col)) +
     geom_histogram(colour = 'white', fill = 'blue', binwidth = bw) +
-    geom_vline(xintercept = outlier_thresholds[1]) +
-    geom_vline(xintercept = outlier_thresholds[2]) +
+    ggtitle(title) +
+    # geom_vline(xintercept = outlier_thresholds[1]) +
+    # geom_vline(xintercept = outlier_thresholds[2]) +
     xlab(cap(
       paste(pretty_var(factor), '[', units(factor), ']', sep = ' '))) +
     ylab('Frequency')
@@ -109,21 +109,21 @@ main <- function () {
   rm(passwd)
   on.exit(dbDisconnect(con))
   
-  
   # Fetch all observations
   # table_name <- 'observations'
-  # factors <- c('pm1', 'pm2_5', 'pm10', 'temperature',
-  #              'humidity', 'pressure')
-
   table_name <- 'meteo_observations'
-  factors <- c('temperature', 'pressure', 'humidity', 'wind_speed',
-               'precip_total', 'precip_rate', 'solradiation')
+  excluded <- c('id', 'station_id')
   query = paste('SELECT * FROM',
                  table_name,
                  sep = ' ')
   obs <- dbGetQuery(con, query)
+  obs <- obs[, !(colnames(obs) %in% excluded)]
   obs[,'date'] <- factor(as.Date(obs$timestamp))
   obs[,'month'] <- as.numeric(format(as.Date(obs$date), '%m'))
+  factors <- colnames(obs)
+  factors <- factors[!(factors %in% c('timestamp'))]
+  month_names <- c('January', 'February', 'March', 'April', 'May', 'June',
+              'July', 'August', 'September', 'October', 'November', 'December')
   
   target_root_dir <- getwd()
   mkdir(target_root_dir)
@@ -140,10 +140,12 @@ main <- function () {
       which <- obs[obs$month == month,]
       plot_name <- paste(factor, '_', month, '.png', sep = '')
       plot_path <- file.path(target_dir, plot_name)
-      save_boxplot(which, factor, plot_path)
+      title <- paste(pretty_var(factor), '  during ', month_names[month])
+      save_boxplot(which, factor, plot_path, title)
       plot_name <- paste('histogram', plot_name, sep = '_')
       plot_path <- file.path(target_dir, plot_name)
-      save_histogram(which, factor, plot_path)
+      title <- paste('Distribution of ', pretty_var(factor), ' during ', month_names[month])
+      save_histogram(which, factor, plot_path, title)
     }
   }
   
@@ -158,7 +160,6 @@ main <- function () {
     # write.csv(stats, file.path(target_dir, paste(factor, '_hourly.csv')))
     # stats <- aggregate(obs[,factor], by = list(obs$date), FUN = stat_fun, na.rm = TRUE)
     # write.csv(stats, file.path(target_dir, paste(factor, '_daily.csv')))
-
     stats <- as.data.frame(
       aggregate(obs[,factor], by = list(obs$date), FUN = mean, na.rm = TRUE))
     names(stats) <- c('date', factor)
